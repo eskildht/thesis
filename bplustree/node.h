e74c2b2d889b76c6b2dde615315a6c1068a0f548
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2021-12-15 12:46:39 +0100

    init

---
+++ bplustree/node.h
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Node {
+	public:
+		bool isLeaf();
+		std::vector<int>& getKeys();
+	private:
+		bool leaf;
+		std::vector<int> keys;
+}
+
+class InternalNode: Node {
+	public:
+		std::vector<Node>& getChildren();
+	private:
+		std::vector<Node> children;
+}
+
+class LeafNode: Node {
+	public:
+		std::vector<int>& getValues(int key);
+	private:
+		std::vector<std::vector<int>> values;
+}
