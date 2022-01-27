c9d7f5c5ab694224aed571b767f8abac3e67c166
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2022-01-28 00:10:14 +0100

    save operation by insert if useBloomFilters

--- parallelbplustree/src/parallelbplustree.cpp
+++ parallelbplustree/src/parallelbplustree.cpp
@@ -163,7 +163,7 @@ std::vector<std::future<void>> ParallelBplustree::updateOrInsert(const int key, 
 			}
 		}
 		if (!keyWasFound) {
-			result.push_back(threadPool.push([this](int id, const int key, const std::vector<int> &values, const int treeIndex) { this->threadUpdateOrInsert(key, values, treeIndex); }, key, values, updateOrInsertTreeSelector));
+			result.push_back(threadPool.push([this](int id, const int key, const std::vector<int> &values, const int treeIndex) { this->threadInsert(key, values, treeIndex); }, key, values, updateOrInsertTreeSelector));
 			updateOrInsertTreeSelector = (updateOrInsertTreeSelector + 1) % numTrees;
 		}
 		return result;
