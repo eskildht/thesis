0d7bf9307bc5978ea165c597f575e56fcad59ccc
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2022-02-01 19:49:58 +0100

    start work on parseUserInput function

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -10,16 +10,37 @@ void printHelpInfo() {
 	std::cout << "\t--help                      " << "Print this help information\n";
 	std::cout << "\n";
 	std::cout << "OPTIONS:\n";
-	std::cout << "\t--build-distr-low <num>     " << "Lowest possible key value during tree build [default: 1]\n";
 	std::cout << "\t--build-distr-high <num>    " << "Highest possible key value during tree build [default: 1000000]\n";
+	std::cout << "\t--build-distr-low <num>     " << "Lowest possible key value during tree build [default: 1]\n";
 	std::cout << "\t--op <num>      " << "Number of operations to perform for the --test value specified [default: 1000000]\n";
-	std::cout << "\t--op-distr-low <num>        " << "Lowest possible key value during test operation [default: 1]\n";
 	std::cout << "\t--op-distr-high <num>       " << "Highest possible key value during test operation [default: 1000000]\n";
+	std::cout << "\t--op-distr-low <num>        " << "Lowest possible key value during test operation [default: 1]\n";
 	std::cout << "\t--order <num>               " << "Order of the Bplustree(s) [default: 5]\n";
 	std::cout << "\t--test <test>               " << "The test to carry out [default: ] [possible values: delete, insert, search, update, updateorinsert]\n";
 	std::cout << "\t--threads <num>             " << "Number of threads to use in the thread pool if --tree has value parallel [default: std::thread::hardware_concurrency()]\n";
 	std::cout << "\t--tree <type>               " << "The tree data structure to create [default: parallel] [possible values: basic, parallel]\n";
 	std::cout << "\t--trees <num>               " << "Number of Bplustrees to use if --tree option has value parallel [default: std::thread::hardware_concurrency()]\n";
+}
+
+std::tuple<std::map<std::string, bool>, std::tuple<std::map<std::string, int>, std::map<std::string, std::string>>> parseUserInput() {
+	std::map<std::string, bool> flagsBool = {
+		{"--bloom-disable", false},
+	 	{"--help", false}
+	};
+	std::map<std::string, int> optionsInt = {
+		{"--build-distr-high", 1000000},
+		{"--build-distr-low", 1},
+		{"--op", 1000000},
+		{"--op-distr-high", 1000000},
+		{"--op-distr-low", 1},
+		{"--order", 5},
+		{"--threads", std::thread::hardware_concurrency()},
+		{"--trees", std::thread::hardware_concurrency()}
+	};
+	std::map<std::string, std::string> optionsString = {
+		{"--test", ""},
+		{"--tree", "parallel"}
+	};
 }
 
 int main(int argc, char *argv[]) {
