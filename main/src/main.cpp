81166eed5c197bfd4be851728f51fd85ebf38c40
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2022-02-02 13:39:25 +0100

    finished main

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -4,7 +4,7 @@
 
 void printHelpInfo() {
 	std::cout << "USAGE:\n";
-	std::cout << "\t./optimized [OPTIONS]\n";
+	std::cout << "\t./optimized [FLAGS] [OPTIONS] --test <test>\n";
 	std::cout << "\n";
 	std::cout << "FLAGS:\n";
 	std::cout << "\t--bloom-disable             " << "Disable bloom filter usage if --tree option has value parallel\n";
@@ -13,20 +13,21 @@ void printHelpInfo() {
 	std::cout << "OPTIONS:\n";
 	std::cout << "\t--build-distr-high <num>    " << "Highest possible key value during tree build [default: 1000000]\n";
 	std::cout << "\t--build-distr-low <num>     " << "Lowest possible key value during tree build [default: 1]\n";
-	std::cout << "\t--op <num>      " << "Number of operations to perform for the --test value specified [default: 1000000]\n";
+	std::cout << "\t--op <num>                  " << "Number of operations to perform for the --test value specified [default: 1000000]\n";
 	std::cout << "\t--op-distr-high <num>       " << "Highest possible key value during test operation [default: 1000000]\n";
 	std::cout << "\t--op-distr-low <num>        " << "Lowest possible key value during test operation [default: 1]\n";
 	std::cout << "\t--order <num>               " << "Order of the Bplustree(s) [default: 5]\n";
 	std::cout << "\t--test <test>               " << "The test to carry out [default: ] [possible values: delete, insert, search, update, updateorinsert]\n";
 	std::cout << "\t--threads <num>             " << "Number of threads to use in the thread pool if --tree has value parallel [default: std::thread::hardware_concurrency()]\n";
 	std::cout << "\t--tree <type>               " << "The tree data structure to create [default: parallel] [possible values: basic, parallel]\n";
+	std::cout << "\t--tree-size <num>           " << "The number of inserts to do during tree build (overridden by --op if --test has value insert) [default: 1000000]\n";
 	std::cout << "\t--trees <num>               " << "Number of Bplustrees to use if --tree option has value parallel [default: std::thread::hardware_concurrency()]\n";
 }
 
-std::tuple<std::map<std::string, bool>, std::tuple<std::map<std::string, int>, std::map<std::string, std::string>>> parseUserInput(int argc, char *argv[]) {
+std::tuple<std::map<std::string, bool>, std::map<std::string, int>, std::map<std::string, std::string>> parseUserInput(int argc, char *argv[]) {
 	std::map<std::string, bool> flagsBool = {
 		{"--bloom-disable", false},
-	 	{"--help", false}
+		{"--help", false}
 	};
 	std::map<std::string, int> optionsInt = {
 		{"--build-distr-high", 1000000},
@@ -36,7 +37,8 @@ std::tuple<std::map<std::string, bool>, std::tuple<std::map<std::string, int>, s
 		{"--op-distr-low", 1},
 		{"--order", 5},
 		{"--threads", std::thread::hardware_concurrency()},
-		{"--trees", std::thread::hardware_concurrency()}
+		{"--trees", std::thread::hardware_concurrency()},
+		{"--tree-size", 1000000}
 	};
 	std::map<std::string, std::string> optionsString = {
 		{"--test", ""},
@@ -46,27 +48,50 @@ std::tuple<std::map<std::string, bool>, std::tuple<std::map<std::string, int>, s
 		{"--test", {"delete", "insert", "search", "update", "updateorinsert"}},
 		{"--tree", {"basic", "parallel"}}
 	};
-	try {
-		for (int i = 1; i < argc; i++) {
-			if (flagsBool.count(argv[i])) {
-				flagsBool[argv[i]] = true;
+	for (int i = 1; i < argc; i++) {
+		if (flagsBool.count(argv[i])) {
+			flagsBool[argv[i]] = true;
+		}
+		else if (optionsInt.count(argv[i])) {
+			optionsInt[argv[i]] = std::stoi(argv[i+1]);
+		}
+		else if (optionsString.count(argv[i])) {
+			std::vector<std::string> &possibleValues = optionsStringPossibleValues[argv[i]];
+			if (std::find(possibleValues.begin(), possibleValues.end(), argv[i+1]) != possibleValues.end()) {
+				optionsString[argv[i]] = argv[i+1];
 			}
-			else if (optionsInt.count(argv[i])) {
-				optionsInt[argv[i]] = std::stoi(argv[i+1]);
-			}
-			else if (optionsString.count(argv[i])) {
-				std::vector<std::string> &possibleValues = optionsStringPossibleValues[argv[i]];
-				if (std::find(possibleValues.begin(), possibleValues.end(), argv[i+1]) != possibleValues.end()) {
-					optionsString[argv[i]] = argv[i+1];
-				}
-				else {
-					std::stringstream msg; 
-					msg << argv[i+1] << " is not a possible value for " << argv[i];
-					throw msg.str();
-				}
+			else {
+				std::stringstream msg;
+				msg << argv[i+1] << " is not a possible value for " << argv[i];
+				throw msg.str();
 			}
 		}
+	}
+	return std::make_tuple(flagsBool, optionsInt, optionsString);
+}
 
+void runProgram(Program &program, const std::string &test, const int op, const int treeSize) {
+	if (test == "delete") {
+		program.deleteTest(op, treeSize);
+	}
+	else if (test == "insert") {
+		program.insertTest(op);
+	}
+	else if (test == "search") {
+		program.searchTest(op, treeSize);
+	}
+	else if (test == "update") {
+		program.updateTest(op, treeSize);
+	}
+	else if (test == "updateorinsert") {
+		program.updateOrInsertTest(op, treeSize);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	std::tuple<std::map<std::string, bool>, std::map<std::string, int>, std::map<std::string, std::string>> result;
+	try {
+		result = parseUserInput(argc, argv);
 	}
 	catch (std::basic_stringstream<char>::string_type errorMsg) {
 		std::cout << errorMsg;
@@ -74,9 +99,25 @@ std::tuple<std::map<std::string, bool>, std::tuple<std::map<std::string, int>, s
 	catch (...) {
 		std::cout << "Error occured during parseUserInput: check flags and options\n";
 	}
-	return std::make_tuple(flagsBool, std::make_tuple(optionsInt, optionsString));
-}
-
-int main(int argc, char *argv[]) {
-	
+	std::map<std::string, bool> &flagsBool = std::get<0>(result);
+	std::map<std::string, int> &optionsInt = std::get<1>(result);
+	std::map<std::string, std::string> &optionsString = std::get<2>(result);
+	if (flagsBool["--help"]) {
+		printHelpInfo();
+		return 0;
+	}
+	else if (optionsString["--test"] == "") {
+		std::cout << "Test to run must be specified using --test option [possible values: delete, insert, search, update, updateorinsert]\n";
+		return 0;
+	}
+	else {
+		if (optionsString["--tree"] == "basic") {
+			Program program(optionsInt["--order"], optionsInt["--op-distr-low"], optionsInt["--op-distr-high"], optionsInt["--build-distr-low"], optionsInt["--build-distr-high"]);
+			runProgram(program, optionsString["--test"], optionsInt["--op"], optionsInt["--tree-size"]);
+		}
+		else {
+			Program program(optionsInt["--order"], optionsInt["--threads"], optionsInt["--trees"], flagsBool["--bloom"], optionsInt["--op-distr-low"], optionsInt["--op-distr-high"], optionsInt["--build-distr-low"], optionsInt["--build-distr-high"]);
+			runProgram(program, optionsString["--test"], optionsInt["--op"], optionsInt["--tree-size"]);
+		}
+	}
 }
