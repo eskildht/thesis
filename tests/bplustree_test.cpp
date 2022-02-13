40eca39012c3d44e5e716efbf1739ed4bf2a98ab
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2022-02-13 21:57:15 +0100

    create Bplustree search tests

--- tests/bplustree_test.cpp
+++ tests/bplustree_test.cpp
@@ -19,3 +19,31 @@ TEST(BplustreeTest, InsertDuplicateKeysTest) {
 		EXPECT_EQ(500, tree.getNumKeysStored());
 	}
 }
+
+TEST(BplustreeTest, SearchForKeysInTreeTest) {
+	Bplustree tree(5);
+	for (int i = 0; i < 500; i++) {
+		tree.insert(i, i + 1);
+	}
+	const std::vector<int> *res;
+	for (int i = 0; i < 30; i += 10) {
+		res = tree.search(i);
+		EXPECT_TRUE(res);
+		EXPECT_EQ(res->size(), 1);
+		EXPECT_EQ((*res)[0], i + 1);
+	}
+	tree.insert(300, 302);
+	res = tree.search(300);
+	EXPECT_EQ(res->size(), 2);
+	EXPECT_EQ((*res)[0], 301);
+	EXPECT_EQ((*res)[1], 302);
+}
+
+TEST(BplustreeTest, SearchForKeyNotInTreeTest) {
+	Bplustree tree(5);
+	for (int i = 0; i < 500; i++) {
+		tree.insert(i, i + 1);
+	}
+	const std::vector<int> *res = tree.search(501);
+	EXPECT_FALSE(res);
+}
