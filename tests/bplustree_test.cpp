9c206299813fcede2662e85a778daf87a21a06f6
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2022-02-14 11:45:51 +0100

    add update tests

--- tests/bplustree_test.cpp
+++ tests/bplustree_test.cpp
@@ -49,3 +49,24 @@ TEST_F(BplustreeTest, SearchForKeyNotInTreeTest) {
 	const std::vector<int> *res = tree.search(1001);
 	EXPECT_FALSE(res);
 }
+
+TEST_F(BplustreeTest, UpdateKeyInTreeTest) {
+	tree.update(500, {1, 2});
+  const std::vector<int> *res = tree.search(500);
+	EXPECT_TRUE(res);
+	EXPECT_EQ(res->size(), 2);
+	EXPECT_EQ((*res)[0], 1);
+	EXPECT_EQ((*res)[1], 2);
+}
+
+TEST_F(BplustreeTest, UpdateKeyNotInTreeTest) {
+	tree.update(1001, {1, 2});
+  const std::vector<int> *res = tree.search(1001);
+	EXPECT_FALSE(res);
+	tree.update(1001, {1, 2}, true);
+  res = tree.search(1001);
+	EXPECT_TRUE(res);
+	EXPECT_EQ(res->size(), 2);
+	EXPECT_EQ((*res)[0], 1);
+	EXPECT_EQ((*res)[1], 2);
+}
